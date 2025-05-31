78d6168389bc84dffb22b352b4a81c444cf4e927
Victor Brønsholm Nielsen <72500348+TheProbPro@users.noreply.github.com>
2025-05-31 23:31:37 +0200

    Delete Bayesian Optimization directory

--- Bayesian Optimization/src/target.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# Target function: models a tumor as a 2D Gaussian bump
-# sigma controls the width of the bump
-# center controls the location of the bump
-# amplitude controls the height of the bump
-def target_function(X, center=(3, 5), sigma=0.5, amplitude=1.0):
-    x, y = X[:, 0], X[:, 1]
-    return amplitude * np.exp(-((x - center[0])**2 + (y - center[1])**2) / (2 * sigma**2))
